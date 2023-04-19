2c26b3cd6a9dc7381079ec0152a43f797932e4d2
Rajeshwarikota <kotarajeswari003.com>
2023-04-19 09:42:47 +0530

    [Rajeshwari kota] Adding ADO.NET project

---
+++ ADO.NetProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".....ADO.NET......");
+            Console.ReadLine();
+        }
+    }
+}
