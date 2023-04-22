1cb2df9a552f12be801a0ae99e4d75a9aeda3fab
“Rajeshwari <kotarajeswari003@gmail.com>
2023-04-22 23:42:26 +0530

    [Rajeshwari kota] Adding Customers

---
+++ ADO.NetProject/CustomerRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADO.NetProject;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.Remoting.Contexts;
+
+namespace ADO.NetProject
+{
+    public class CustomerRepository
+    {
+        public static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = ADONET;Integrated Security = True;Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent = ReadWrite; MultiSubnetFailover=False";
+        public static void AddCustomer(Customer model)
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+
+                SqlCommand cmd = new SqlCommand("SPADDCUSTOMERS", sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                cmd.Parameters.AddWithValue("@Name", model.Name);
+                cmd.Parameters.AddWithValue("@City", model.City);
+                cmd.Parameters.AddWithValue("@Address", model.Address);
+                cmd.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                cmd.Parameters.AddWithValue("@Salary", model.Salary);
+
+                int num = cmd.ExecuteNonQuery();
+                if (num != 0)
+                {
+                    Console.WriteLine("Customer added Successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Something went Wrong");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- ADO.NetProject/Program.cs
+++ ADO.NetProject/Program.cs
@@ -11,7 +11,31 @@ namespace ADO.NetProject
         static void Main(string[] args)
         {
             Console.WriteLine(".....ADO.NET......");
-            Console.ReadLine();
+            Console.WriteLine("Select any one Option for \n" +
+               "1. Add customer details into customer table");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Customer customer = new Customer();
+                    Console.WriteLine("Enter Customer Name");
+                    customer.Name = Console.ReadLine();
+                    Console.WriteLine("Enter Customer City");
+                    customer.City = Console.ReadLine();
+                    Console.WriteLine("Enter Customer Address");
+                    customer.Address = Console.ReadLine();
+                    Console.WriteLine("Enter Customer Phone Number");
+                    customer.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter Customer Salary");
+                    customer.Salary = Convert.ToInt64(Console.ReadLine());
+
+                    CustomerRepository.AddCustomer(customer);
+                    break;
+                default:
+                    Console.WriteLine("Please Select Correct Option");
+                    break;
+            }
+                    Console.ReadLine();
         }
     }
 }
